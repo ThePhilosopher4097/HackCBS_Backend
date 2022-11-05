8e0b9f6b00e6b64dfb502963d2fde301e1586562
sameer <santdnyanrajmauli@gmail.com>
2022-11-05 17:57:37 +0530

    testing apis

--- Server.js
+++ Server.js
@@ -6,5 +6,9 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
+app.use('./api/register', require('./api/register'))
+app.use('./api/login', require('./api/login'))
+app.use('./api/userdocs', require('./api/getUserDocuments'))
+
 app.listen(port, () => console.log(`Server started on Port ${port}`))
 console.log('Xggpians Doc-Share')
---
+++ api/register.js
@@ -0,0 +1,18 @@
+
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (request, response) => {
+    response.send({message:'Register api test'})
+})
+router.get('/', (request, response) => {
+    response.send({message:'Register api test'})
+})
+router.get('/', (request, response) => {
+    response.send({message:'Register api test'})
+})
+router.get('/', (request, response) => {
+    response.send({message:'Register api test'})
+})
+
+module.exports = router
