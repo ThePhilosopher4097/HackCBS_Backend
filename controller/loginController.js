9f608c44188bcbb7494011489257ba56d513a61d
sameer <santdnyanrajmauli@gmail.com>
2022-11-05 20:32:01 +0530

    APi test

--- controller/loginController.js
+++ controller/loginController.js
@@ -2,11 +2,14 @@ const express = require('express')
 const router = express.Router()
 
 // @desc Login the user
-// @route POST /api/login
+// @route GET /api/login/validate
 // @access Private
 const validateUser = (request, response) => {
     response.status(200).json({message:'Success : User Exists !'})
 }
+// @desc Login the user
+// @route GET /api/login/verify
+// @access Private
 const verifyUser = (request, response) => {
     response.status(200).json({message:'Success : User Logged in !'})
 }
