209e59d156d4b66eaba8088fadfdfc7c16a3f385
sameer <santdnyanrajmauli@gmail.com>
2022-11-06 00:18:04 +0530

    APIs test

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
+const MONGO_URI = 'mongodb+srv://xgppians:xgppians@xgppians.hse6du6.mongodb.net/?retryWrites=true&w=majority'
 
 const connectDB = async() => {
     try{
-        const connection = await mongoose.connect(process.env.MONGO_URI)
+        const connection = await mongoose.connect(MONGO_URI)
         console.log(`Mongo DB Connected : ${connection.connection.host}`.cyan.underline)
     } catch(error) {
         console.log(error)
