32b27ad0109006dc1d1dde042dcced12b049e2e5
sameer <santdnyanrajmauli@gmail.com>
2022-11-05 22:03:33 +0530

    error handler

--- Server.js
+++ Server.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+const erroHandler = require('./middleware/errorMiddleware')
 
 const app = express()
 
@@ -10,6 +11,10 @@ const app = express()
     console.log('Hello !')
     res.status(200).json({message:'Okay!'})
 })*/
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
 app.use('/api/register', require('./api/register'))
 app.use('/api/login', require('./api/login'))
 //app.use('./api/userdocs', require('./api/getUserDocuments'))
--- controller/registerController.js
+++ controller/registerController.js
@@ -5,6 +5,9 @@ const router = express.Router()
 // @route POST /api/register
 // @access Private
 const registerUser = (request, response) => {
+    if (!request.body.text) {
+        return response.status(400).json({message:'Please add data.'})    
+    }
     response.status(200).json({message:'Success : User Registered !'})
 }
 
