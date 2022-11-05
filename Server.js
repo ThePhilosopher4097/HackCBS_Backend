330614e09dee04196ac901e7b8bd0ec2b949939c
sameer <santdnyanrajmauli@gmail.com>
2022-11-05 17:25:18 +0530

    configs added

---
+++ Server.js
@@ -0,0 +1,10 @@
+
+// Modules
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.listen(port, () => console.log(`Server started on Port ${port}`))
+console.log('Xggpians Doc-Share')
