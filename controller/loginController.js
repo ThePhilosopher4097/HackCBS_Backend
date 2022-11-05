110e7066ee6c2bb1b986bafef8cd617c62652b0e
sameer <santdnyanrajmauli@gmail.com>
2022-11-05 22:13:27 +0530

    APi test

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const asyncHandler = require('express-async-handler')
 
 // @desc Login the user
 // @route GET /api/login/validate
 // @access Private
-const validateUser = (request, response) => {
+const validateUser = asyncHandler( async (request, response) => {
     response.status(200).json({message:'Success : User Exists !'})
-}
+})
 // @desc Login the user
 // @route GET /api/login/verify
 // @access Private
-const verifyUser = (request, response) => {
+const verifyUser = asyncHandler( async (request, response) => {
     response.status(200).json({message:'Success : User Logged in !'})
-}
+})
 
 module.exports = {
     validateUser,
--- controller/registerController.js
+++ controller/registerController.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const asyncHandler = require('express-async-handler')
 
 // @desc Register the user
 // @route POST /api/register
 // @access Private
-const registerUser = (request, response) => {
+const registerUser = asyncHandler( async (request, response) => {
     if (!request.body.text) {
         return response.status(400).json({message:'Please add data.'})    
     }
     response.status(200).json({message:'Success : User Registered !'})
-}
+})
 
 module.exports = {
     registerUser
